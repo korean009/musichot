9c488ffebfe07db1e5f18382590d097587392e1c
BADNAM XD <yadavxd746@gmail.com>
2025-12-06 10:26:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots790d34') # youtube song api ke 
+API_KEY = getenv("API_KEY", '30DxNexGenBots743082') # youtube song api ke 
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
@@ -121,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
